603f74095b4413b0d4d9b498752d0e987db57043
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-06-19 19:10:29 +0300

    commit on 'master'

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(driverNames, matchingName) {
+    function isMatchingName(driverName) {
+        return driverName.toLowerCase() === matchingName.toLowerCase();
+    }
+
+    return driverNames.filter(isMatchingName);
+}
+
+function fuzzyMatch(driverNames, matchingName) {
+    function isMatchingLetters(driverName) {
+        return driverName.startsWith(matchingName)
+    }
+
+    return driverNames.filter(isMatchingLetters)
+}
+
+function matchName(driver, string) {
+    function isMatchingName(driverName){
+        return driverName.name.toLowerCase() === string.toLowerCase(); 
+    }
+
+    return driver.filter(isMatchingName);
+}
